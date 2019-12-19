916312a14a326e6e8b5738f219f17fce021bc4b7
vagrant <vagrant@vagrant.vm>
2019-12-19 13:50:49 +0000

    Update since 12/17

--- makedataset.py
+++ makedataset.py
@@ -8,7 +8,7 @@ def tsv2txt(torv, s_line):
             f.write(s[2]+'\t'+s[6]+'\t'+s[9]+'\n')
 
 # Main
-with open('./material/training_set_rel3.tsv', encoding="utf8", errors='ignore') as f:
+with open('./asap-aes/tmp.tsv', encoding="utf8", errors='ignore') as f:
     line_cnt = 0
     for s_line in f:
         if line_cnt == 0:
@@ -22,7 +22,7 @@ with open('./material/training_set_rel3.tsv', encoding="utf8", errors='ignore') 
 
 
 score = {}
-with open('./material/valid_score.txt', 'rb') as f:
+with open('./asap-aes/valid_score.txt', 'rb') as f:
     line_cnt = 1
     for s_line in f:
         if line_cnt == 1:
@@ -33,7 +33,7 @@ with open('./material/valid_score.txt', 'rb') as f:
         score[key] = x[4].strip()
         line_cnt += 1
 
-with open('./material/valid_set.tsv', encoding="utf8", errors='ignore') as f:
+with open('./asap-aes/tmp_valid.tsv', encoding="utf8", errors='ignore') as f:
     line_cnt = 0
     for s_line in f:
         if line_cnt == 0:
@@ -43,9 +43,9 @@ with open('./material/valid_set.tsv', encoding="utf8", errors='ignore') as f:
         with open('./dataset/test_essay'+s[1]+'.txt', 'a') as f:
             if x[1] != '2':
                 sc = score[s[3]+','+s[0]]
-                f.write(x[2]+'\t'+sc+'\n')
+                f.write(s[2]+'\t'+sc+'\n')
             else:
-                s1 = score[x[3]+','+x[0]]
-                s2 = score[x[4].strip()+','+x[0]]
-                f.write(x[2]+'\t'+s1+'\t'+s2+'\n')
+                s1 = score[s[3]+','+s[0]]
+                s2 = score[s[4].strip()+','+s[0]]
+                f.write(s[2]+'\t'+s1+'\t'+s2+'\n')
         line_cnt += 1
